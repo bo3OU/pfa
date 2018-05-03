105c089f9f0993d6df42ae12158d4151d36cf473
ali bagho <ali.bagho@gmail.com>
2018-05-03 01:02:25 +0100

    Testing git branching

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -497,7 +497,7 @@ class Dashboard extends Component {
                     </DropdownMenu>
                   </ButtonDropdown>
                 </ButtonGroup>
-                <div className="text-value">9.823</div>
+                <div className="text-value">testing :D</div>
                 <div>Members online</div>
               </CardBody>
               <div className="chart-wrapper mx-3" style={{ height: '70px' }}>
