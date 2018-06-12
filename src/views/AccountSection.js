8b21c7b9b4f315cd2b204382e03c743b117f9f40
ali bagho <ali.bagho@gmail.com>
2018-06-12 05:18:41 +0000

    initial work on app's headbar

---
+++ src/views/AccountSection.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { UncontrolledDropdown,  DropdownItem, DropdownMenu, DropdownToggle} from 'reactstrap';
+import request from 'request';
+import consts from '../consts';
+
+class AccountSection extends Component {
+  constructor(props) {
+    super(props)
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+
+  componentDidMount() {
+    request.get(consts.url + "").then(function() {
+        this.setState({
+      
+        });
+    });
+
+  }
+
+  render() {
+
+    return (
+        //TODO if he is logged in !!if not simple log in button
+         <UncontrolledDropdown nav inNavbar>
+           <DropdownToggle nav caret>
+             Account
+           </DropdownToggle>
+           <DropdownMenu right>
+             <DropdownItem>
+               Favorites
+             </DropdownItem>
+             <DropdownItem>
+               Settings
+             </DropdownItem>
+             <DropdownItem>
+               Log out
+             </DropdownItem>
+           </DropdownMenu>
+         </UncontrolledDropdown>
+    );
+  }
+}
+
+Pfaheader.propTypes = propTypes;
+Pfaheader.defaultProps = defaultProps;
+
+export default AccountSection;
---
+++ src/views/SearchCoin.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { Collapse, UncontrolledDropdown, NavbarToggler, Badge, DropdownItem, DropdownMenu, DropdownToggle, Nav, NavItem, NavLink, NavbarBrand } from 'reactstrap';
+import PropTypes from 'prop-types';
+import request from 'request';
+import consts from '../consts';
+
+class SearchCoin extends Component {
+  	constructor(props) {
+		super(props)
+		this.state = {
+			searchValue: ""
+		}
+  	}
+
+	Search() {
+		//TODO
+		request.get(consts.url + "search/");
+		this.setState({
+			Data: []
+		});
+	}
+	Change() {
+		
+	}
+	render() {
+		return (
+			<React.Fragment>
+			<input onChange={event => this.setState({searchValue: event.target.value})}>
+
+			</input>
+
+			</React.Fragment>
+		);
+	}
+}
+export default SearchCoin;
